47d9caa6b77acd7b1383b34720412d47e4dde144
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2018-04-16 13:41:43 +0200

    Use open command if on macOS (opens folder and highlights file)

--- src/main/java/net/ralphbln/protege/plugin/sourcelocation/menu/OpenFileLocationAction.java
+++ src/main/java/net/ralphbln/protege/plugin/sourcelocation/menu/OpenFileLocationAction.java
@@ -27,8 +27,34 @@ public class OpenFileLocationAction extends ProtegeOWLAction {
 		if (scheme == null || scheme.equalsIgnoreCase("file")) {
 			File file = new File(documentURI);
 			if (file.exists()) {
-				if (!file.isDirectory()) {
-					documentURI = new File(file.getParent()).toURI();
+
+				if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+					// on a Mac, we have even nicer options
+
+					String[] cmdArray;
+
+					File workingDirectory;
+
+					if (file.isDirectory()) {
+						// should never happen, but just in case
+						workingDirectory = file;
+						cmdArray = new String[] {"open", file.getAbsolutePath()};
+					} else {
+						workingDirectory = file.getParentFile();
+						cmdArray = new String[] {"open", "-R", file.getAbsolutePath()};
+					}
+					try {
+						Runtime.getRuntime().exec(cmdArray, null, workingDirectory);
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+						return;
+					}
+				} else {
+					if (!file.isDirectory()) {
+						// should always be a file, but just in case
+						documentURI = new File(file.getParent()).toURI();
+					}
 				}
 			}
 		}
